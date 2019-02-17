72b0ecd5b1c02e292de0e50114b4632666dd46af
Sebastien Guimont <info@sebastienguimont.com>
2019-02-17 17:30:53 -0500

    Revert "Initial update"
    This reverts commit 65fa3020

--- do.bat
+++
@@ -1,7 +0,0 @@
-node . > services.json
-call services-deps -i output/service-dot.png --stdin < services.json
-call services-deps -i output/service-neato.png -l neato --stdin < services.json
-call services-deps -i output/service-fdp.png l fdp --stdin < services.json
-call services-deps -i output/service-sfpd.png l sfdp --stdin < services.json
-call services-deps -i output/service-twopi.png l twopi --stdin < services.json
-call services-deps -i output/service-circo.png l circo --stdin < services.json
