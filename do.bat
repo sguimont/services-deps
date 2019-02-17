d647c83ea766500a92d3b7e00e8509665bfc01cb
Sebastien Guimont <info@sebastienguimont.com>
2019-02-17 17:43:07 -0500

    Adjust the batch

--- do.bat
+++ do.bat
@@ -5,3 +5,4 @@ call services-deps -i output/service-fdp.png l fdp --stdin < services.json
 call services-deps -i output/service-sfpd.png l sfdp --stdin < services.json
 call services-deps -i output/service-twopi.png l twopi --stdin < services.json
 call services-deps -i output/service-circo.png l circo --stdin < services.json
+call services-deps --orphans -s -c --stdin < services.json
